738084a617c988d8544ef71933deee5ee4c01b49
jimmy-gnahz <jimmyjimbozhang@gmail.com>
2021-02-12 16:01:14 +0800

    Initial Game Setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using SVS;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public CameraMovement cameraMovement;
+    //public RoadManager roadManager;
+    public InputManager inputManager;
+
+
+    private void Start()
+    {
+        inputManager.OnMouseClick += HandleMouseClick; // for action
+    }
+
+    private void HandleMouseClick(Vector3Int position)
+    {
+        Debug.Log(position);
+    }
+
+    private void Update()
+    {
+        cameraMovement.MoveCamera(new Vector3(inputManager.CameraMovementVector.x, 0, inputManager.CameraMovementVector.y));
+    }
+}
